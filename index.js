7792204c99829b3d32aa783d44fff14e317ae3ee
Luka Berukashvili <lukaberukashvili@gmail.com>
2023-03-03 17:35:09 +0400

    add user list and data

---
+++ index.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  // Get User Data From API
+  $.getJSON("https://jsonplaceholder.typicode.com/users", function (users) {
+    // Iterate Over Users And Append To List
+    $.each(users, function (index, user) {
+      const listItem = $("<a>")
+        .addClass("list-group-item list-group-item-action")
+        .attr("href", "user.html?id=" + user.id)
+        .text(user.name);
+      $("#user-list").append(listItem);
+    });
+  });
+});

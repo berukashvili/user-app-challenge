efef4e9470ee66d5a087a6c4f54cb223958b6f2c
Luka Berukashvili <lukaberukashvili@gmail.com>
2023-03-03 21:17:21 +0400

    add fetched data to user page

---
+++ user.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  // Get ID
+  const userId = new URLSearchParams(window.location.search).get("id");
+
+  // Get user data from API
+  $.getJSON(
+    "https://jsonplaceholder.typicode.com/users/" + userId,
+    function (user) {
+      // Inject Data
+      $("#user-name").text(user.name);
+      $("#user-email").text(user.email);
+      $("#user-phone").text(user.phone);
+      $("#user-website").text(user.website);
+      $("#user-address").text(user.address.street + ", " + user.address.suite);
+      $("#user-city").text(user.address.city);
+      $("#user-company").text(user.company.name);
+      $("#user-company-catchphrase").text(user.company.catchPhrase);
+    }
+  );
+});
